745936c6ecf3661d6324ce6413dbd1159187c7b8
Pascal Jäger <pascal.jaeger@gmx.de>
2011-12-05 07:03:39 +0100

    Anruflogik an SipLayer Klasse weitergeleitet und Beispielcode eingefügt.

--- SIP Beispiel Client/SIP Beispiel Client/src/dev2dev/textclient/TextClient.java
+++ SIP Beispiel Client/SIP Beispiel Client/src/dev2dev/textclient/TextClient.java
@@ -42,189 +42,177 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class TextClient 
-	extends JFrame
-	implements MessageProcessor
-{
-    private SipLayer sipLayer;
-    
-    private JTextField fromAddress;
-    private JLabel fromLbl;
-    private JLabel receivedLbl;
-    private JTextArea receivedMessages;
-    private JScrollPane receivedScrollPane;
-    private JButton sendBtn;
-    private JButton callBtn;
-    private JLabel sendLbl;
-    private JTextField sendMessages;
-    private JTextField toAddress;
-    private JLabel toLbl;
-	
-    public static void main(String[] args)
-    {
-        if(args.length != 2) {
-            printUsage();
-            System.exit(-1);            
-        }
-        
-		try
-        {
-		    String username = args[0];
-		    int port = Integer.parseInt(args[1]);
-		    String ip = InetAddress.getLocalHost().getHostAddress();
+public class TextClient extends JFrame implements MessageProcessor {
+	private SipLayer sipLayer;
+
+	private JTextField fromAddress;
+	private JLabel fromLbl;
+	private JLabel receivedLbl;
+	private JTextArea receivedMessages;
+	private JScrollPane receivedScrollPane;
+	private JButton sendBtn;
+	private JButton callBtn;
+	private JLabel sendLbl;
+	private JTextField sendMessages;
+	private JTextField toAddress;
+	private JLabel toLbl;
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			printUsage();
+			System.exit(-1);
+		}
+
+		try {
+			String username = args[0];
+			int port = Integer.parseInt(args[1]);
+			String ip = InetAddress.getLocalHost().getHostAddress();
 
 			SipLayer sipLayer = new SipLayer(username, ip, port);
-		    TextClient tc = new TextClient(sipLayer);
-		    sipLayer.setMessageProcessor(tc);
-			
+			TextClient tc = new TextClient(sipLayer);
+			sipLayer.setMessageProcessor(tc);
+
 			tc.show();
-        } catch (Throwable e)
-        {
-            System.out.println("Problem initializing the SIP stack.");
-            e.printStackTrace();
-            System.exit(-1);
-        }
-    }
-
-    private static void printUsage()
-    {
-        System.out.println("Syntax:");
-        System.out.println("  java -jar textclient.jar <username> <port>");
-        System.out.println("where <username> is the nickname of this user");
-        System.out.println("and <port> is the port number to use. Usually 5060 if not used by another process.");
-        System.out.println("Example:");
-        System.out.println("  java -jar textclient.jar snoopy71 5061");
-    }
-
-    public TextClient(SipLayer sip)
-    {
-        super();
-        sipLayer = sip;
-        initWindow();
-        String from = "sip:" + sip.getUsername() + "@" + sip.getHost() + ":" + sip.getPort();
-        this.fromAddress.setText(from);
-    }
-    
-    private void initWindow() {
-        receivedLbl = new JLabel();
-        sendLbl = new JLabel();
-        sendMessages = new JTextField();
-        receivedScrollPane = new JScrollPane();
-        receivedMessages = new JTextArea();
-        fromLbl = new JLabel();
-        fromAddress = new JTextField();
-        toLbl = new JLabel();
-        toAddress = new JTextField();
-        sendBtn = new JButton();
-        callBtn = new JButton();
-
-        getContentPane().setLayout(null);
-
-        setTitle("TextClient");
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent evt) {
-                System.exit(0);
-            }
-        });
-
-        receivedLbl.setText("Received Messages:");
-        receivedLbl.setAlignmentY(0.0F);
-        receivedLbl.setPreferredSize(new java.awt.Dimension(25, 100));
-        getContentPane().add(receivedLbl);
-        receivedLbl.setBounds(5, 0, 136, 20);
-
-        sendLbl.setText("Send Message:");
-        getContentPane().add(sendLbl);
-        sendLbl.setBounds(5, 150, 90, 20);
-
-        getContentPane().add(sendMessages);
-        sendMessages.setBounds(5, 170, 270, 20);
-
-        receivedMessages.setAlignmentX(0.0F);
-        receivedMessages.setEditable(false);
-        receivedMessages.setLineWrap(true);
-        receivedMessages.setWrapStyleWord(true);
-        receivedScrollPane.setViewportView(receivedMessages);
-        receivedScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-        getContentPane().add(receivedScrollPane);
-        receivedScrollPane.setBounds(5, 20, 270, 130);
-
-        fromLbl.setText("From:");
-        getContentPane().add(fromLbl);
-        fromLbl.setBounds(5, 200, 35, 15);
-
-        getContentPane().add(fromAddress);
-        fromAddress.setBounds(40, 200, 235, 20);
-        fromAddress.setEditable(false);
-
-        toLbl.setText("To:");
-        getContentPane().add(toLbl);
-        toLbl.setBounds(5, 225, 35, 15);
-
-        getContentPane().add(toAddress);
-        toAddress.setBounds(40, 225, 235, 21);
-
-        sendBtn.setText("Send");
-        sendBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                sendBtnActionPerformed(evt);
-            }
-        });
-
-        getContentPane().add(sendBtn);
-        sendBtn.setBounds(200, 255, 75, 25);
-
-        
-        callBtn.setText("Call");
-        callBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                callBtnActionPerformed(evt);
-            }
-        });
-
-        getContentPane().add(callBtn);
-        callBtn.setBounds(1, 255, 75, 25);
-
-        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds((screenSize.width-288)/2, (screenSize.height-310)/2, 288, 320);
-    }
-
-    private void sendBtnActionPerformed(ActionEvent evt) {
-
-        try
-        {
-            String to = this.toAddress.getText();
-            String message = this.sendMessages.getText();
-            sipLayer.sendMessage(to, message);
-        } catch (Throwable e)
-        {
-            e.printStackTrace();
-            this.receivedMessages.append("ERROR sending message: " + e.getMessage() + "\n");
-        }
-        			
-    }
-    
-    private void callBtnActionPerformed(ActionEvent evt) {
-		System.out.println("Calling...");
-		
+		} catch (Throwable e) {
+			System.out.println("Problem initializing the SIP stack.");
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 
-    public void processMessage(String sender, String message)
-    {
-        this.receivedMessages.append("From " +
-                sender + ": " + message + "\n");
-    }
+	private static void printUsage() {
+		System.out.println("Syntax:");
+		System.out.println("  java -jar textclient.jar <username> <port>");
+		System.out.println("where <username> is the nickname of this user");
+		System.out
+				.println("and <port> is the port number to use. Usually 5060 if not used by another process.");
+		System.out.println("Example:");
+		System.out.println("  java -jar textclient.jar snoopy71 5061");
+	}
 
-    public void processError(String errorMessage)
-    {
-        this.receivedMessages.append("ERROR: " +
-                errorMessage + "\n");
-    }
-    
-    public void processInfo(String infoMessage)
-    {
-        this.receivedMessages.append(
-                infoMessage + "\n");
-    }
+	public TextClient(SipLayer sip) {
+		super();
+		sipLayer = sip;
+		initWindow();
+		String from = "sip:" + sip.getUsername() + "@" + sip.getHost() + ":"
+				+ sip.getPort();
+		this.fromAddress.setText(from);
+	}
+
+	private void initWindow() {
+		receivedLbl = new JLabel();
+		sendLbl = new JLabel();
+		sendMessages = new JTextField();
+		receivedScrollPane = new JScrollPane();
+		receivedMessages = new JTextArea();
+		fromLbl = new JLabel();
+		fromAddress = new JTextField();
+		toLbl = new JLabel();
+		toAddress = new JTextField();
+		sendBtn = new JButton();
+		callBtn = new JButton();
+
+		getContentPane().setLayout(null);
+
+		setTitle("TextClient");
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				System.exit(0);
+			}
+		});
+
+		receivedLbl.setText("Received Messages:");
+		receivedLbl.setAlignmentY(0.0F);
+		receivedLbl.setPreferredSize(new java.awt.Dimension(25, 100));
+		getContentPane().add(receivedLbl);
+		receivedLbl.setBounds(5, 0, 136, 20);
+
+		sendLbl.setText("Send Message:");
+		getContentPane().add(sendLbl);
+		sendLbl.setBounds(5, 150, 90, 20);
+
+		getContentPane().add(sendMessages);
+		sendMessages.setBounds(5, 170, 270, 20);
+
+		receivedMessages.setAlignmentX(0.0F);
+		receivedMessages.setEditable(false);
+		receivedMessages.setLineWrap(true);
+		receivedMessages.setWrapStyleWord(true);
+		receivedScrollPane.setViewportView(receivedMessages);
+		receivedScrollPane
+				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		getContentPane().add(receivedScrollPane);
+		receivedScrollPane.setBounds(5, 20, 270, 130);
+
+		fromLbl.setText("From:");
+		getContentPane().add(fromLbl);
+		fromLbl.setBounds(5, 200, 35, 15);
+
+		getContentPane().add(fromAddress);
+		fromAddress.setBounds(40, 200, 235, 20);
+		fromAddress.setEditable(false);
+
+		toLbl.setText("To:");
+		getContentPane().add(toLbl);
+		toLbl.setBounds(5, 225, 35, 15);
+
+		getContentPane().add(toAddress);
+		toAddress.setBounds(40, 225, 235, 21);
+
+		sendBtn.setText("Send");
+		sendBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				sendBtnActionPerformed(evt);
+			}
+		});
+
+		getContentPane().add(sendBtn);
+		sendBtn.setBounds(200, 255, 75, 25);
+
+		callBtn.setText("Call");
+		callBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				callBtnActionPerformed(evt);
+			}
+		});
+
+		getContentPane().add(callBtn);
+		callBtn.setBounds(1, 255, 75, 25);
+
+		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
+				.getScreenSize();
+		setBounds((screenSize.width - 288) / 2, (screenSize.height - 310) / 2,
+				288, 320);
+	}
+
+	private void sendBtnActionPerformed(ActionEvent evt) {
+
+		try {
+			String to = this.toAddress.getText();
+			String message = this.sendMessages.getText();
+			sipLayer.sendMessage(to, message);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			this.receivedMessages.append("ERROR sending message: "
+					+ e.getMessage() + "\n");
+		}
+	}
+
+	private void callBtnActionPerformed(ActionEvent evt) {
+		System.out.println("Calling...");
+		sipLayer.startCall();
+	}
+
+	public void processMessage(String sender, String message) {
+		this.receivedMessages.append("From " + sender + ": " + message + "\n");
+	}
+
+	public void processError(String errorMessage) {
+		this.receivedMessages.append("ERROR: " + errorMessage + "\n");
+	}
+
+	public void processInfo(String infoMessage) {
+		this.receivedMessages.append(infoMessage + "\n");
+	}
 }
